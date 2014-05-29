eef17a3ecee3838f1987d93c99fbcc88c4b9df29
pugwoo <pugwoo@gmail.com>
2014-05-29 16:47:06 +0800

    一些form注入的说明

--- spring_mvc3/src/main/java/spring_mvc3/web/CplxFormController.java
+++ spring_mvc3/src/main/java/spring_mvc3/web/CplxFormController.java
@@ -1,13 +1,14 @@
 package spring_mvc3.web;
 
-import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.Writer;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import spring_mvc3.web.form.CplxForm;
+import spring_mvc3.web.form.Student;
 
 /**
  * 演示页面传递大量数据例子
@@ -43,6 +44,9 @@ public class CplxFormController {
 	 * (2014年5月28日)注: 不要用这种方法，不然form注入就被调用了3次!而且容易出现类型不匹配!
 	 * 找到一种完美的解决方法：把@ModelAttribute("cplxForm")方法里的变量保存到类的成员变量，
 	 * 然后@RequestMapping不要用CplxForm form来注入就可以~!
+	 * 
+	 * 【这种方法实际上还是不好的】不要像Controller里面引入成员变量，因为它是单例的!
+	 * 对于大表单form提交的，还是用form对象来放数据，然后再转成entity对象
 	 * @param form
 	 * @param model
 	 * @return
@@ -55,5 +59,19 @@ public class CplxFormController {
 		model.addAttribute("students_num", form.getStudents() == null ? 0
 				: form.getStudents().size());
 		return "form_result";
+	}
+	
+	/**
+	 * 注：student无论如何不会是null
+	 * 当参数中连name=都没有时，student中的name就是null
+	 * 当参数中有name=，则student中的name就是空的（不是null）
+	 * @param student
+	 * @param writer
+	 * @throws IOException
+	 */
+	@RequestMapping("/student_post")
+	public void postStudent(Student student, Writer writer) throws IOException {
+		System.out.println("==" + student + "," + student.getId() + "," + student.getName());
+		writer.write("ok");
 	}
 }
