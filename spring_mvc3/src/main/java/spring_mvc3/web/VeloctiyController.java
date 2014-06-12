97382100dfeb17893bc7f4694c15e237a3f5c839
pugwoo <pugwoo@gmail.com>
2014-06-12 14:05:51 +0800

    velocity test

--- spring_mvc3/src/main/java/spring_mvc3/web/VeloctiyController.java
+++ spring_mvc3/src/main/java/spring_mvc3/web/VeloctiyController.java
@@ -1,5 +1,10 @@
 package spring_mvc3.web;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +27,37 @@ public class VeloctiyController {
 		
 		model.addAttribute("student", student);
 		
-		return "index";
+		return "velocity_index";
+	}
+	
+	@RequestMapping(value = "/velocity_condition")
+	public String condition(Model model) {
+		
+		model.addAttribute("userName", "nick");
+		
+		Student student = new Student();
+		student.setId(3L);
+		student.setName("NICK");
+		
+		model.addAttribute("student", student);
+		
+		return "velocity_condition";
+	}
+	
+	@RequestMapping(value = "/velocity_loop")
+	public String loop(Model model) {
+		
+		List<String> list = new ArrayList<String>();
+		list.add("haha");
+		list.add("you_df");
+		list.add("us");
+		model.addAttribute("list", list);
+		
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("eleven", 11);
+		map.put("three", 3);
+		model.addAttribute("map", map);
+		
+		return "velocity_loop";
 	}
 }
