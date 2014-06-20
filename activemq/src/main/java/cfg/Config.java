470ab50a3344ab59622be09661bd622fdd951b0c
pugwoo <pugwoo@gmail.com>
2014-06-20 22:51:49 +0800

    Add Config.java to .gitignore

---
+++ activemq/src/main/java/cfg/Config.java
@@ -0,0 +1,11 @@
+package cfg;
+
+/**
+ * 一些配置信息
+ */
+public class Config {
+
+	public static String url = "tcp://vpstx.pugwoo.com:61616";
+	public static String user = "admin";
+	public static String password = "admin"; // TODO 
+}
---
+++ redis_client/src/main/java/com/pugwoo/practice/redis_client/Config.java
@@ -0,0 +1,6 @@
+package com.pugwoo.practice.redis_client;
+
+public class Config {
+
+	public static String host = "vps.pugwoo.com";
+}
