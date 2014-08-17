bb5c9ad8096071f338f7882650c16bcf00a9595f
pugwoo <pugwoo@gmail.com>
2014-08-17 22:57:01 +0800

    namespace

--- ibatis/src/test/java/com/pugwoo/practice/ibatis/TestOperatingDB.java
+++ ibatis/src/test/java/com/pugwoo/practice/ibatis/TestOperatingDB.java
@@ -9,14 +9,23 @@ import com.ibatis.sqlmap.client.SqlMapClient;
 import com.ibatis.sqlmap.client.SqlMapClientBuilder;
 import com.pugwoo.practice.ibatis.entity.Student;
 
+/**
+ * 注意：如果要使用ibatis的namespace特性，记得sqlmapConfig.xml中的settings
+ * 设置useStatementNamespaces为true。此时就只能使用namespace的方式，二选一。
+ *
+ */
 public class TestOperatingDB {
 
 	public static void main(String[] args) throws SQLException, IOException {
 		Reader reader = Resources.getResourceAsReader("SqlMapConfig.xml");
 		SqlMapClient sqlMap = SqlMapClientBuilder.buildSqlMapClient(reader);
 		
-		Student student = (Student) sqlMap.queryForObject("getStudentById", 1);
+		Student student = (Student) sqlMap.queryForObject("Student.getStudentById", 1);
 		System.out.println(student);
+		
+		// 启用namespace之后就只能用上面那种，一般大型项目都会用namespace的
+//		student = (Student) sqlMap.queryForObject("getStudentById", 1);
+//		System.out.println(student);
 	}
 
 }
