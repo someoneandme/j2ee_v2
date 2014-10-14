b30cebc8e111b130550c45e74ddab888e2618be9
pugwoo <pugwoo@gmail.com>
2014-10-14 21:25:25 +0800

    spring async

---
+++ spring_basic/src/main/java/spring_basic/async/SomeMethod.java
@@ -0,0 +1,29 @@
+package spring_basic.async;
+
+import java.util.concurrent.Future;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Component;
+
+/**
+ * 2014年10月14日 21:06:32
+ */
+@Component
+public class SomeMethod {
+
+	@Async
+	public void sayHello() throws InterruptedException {
+		System.out.println("will sleep 3s");
+		Thread.sleep(3000);
+		System.out.println("has sleep 3s");
+	}
+	
+	@Async
+	public Future<String> sayHello(String name) throws InterruptedException {
+		String value = "hello," + name;
+		Thread.sleep(3000);
+		return new AsyncResult<String>(value);
+	}
+	
+}
---
+++ spring_basic/src/test/java/spring_basic/aop/test/TestAysnc.java
@@ -0,0 +1,33 @@
+package spring_basic.aop.test;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import spring_basic.async.SomeMethod;
+
+/**
+ * 2014年10月14日 21:07:40
+ */
+public class TestAysnc {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		String xmlPath = "/applicationContext-async.xml";
+		ApplicationContext context = new ClassPathXmlApplicationContext(xmlPath);
+
+		SomeMethod someMethod = context.getBean(SomeMethod.class);
+
+		System.out.println("before call somemethod");
+		someMethod.sayHello();
+		System.out.println("after call somemethod");
+
+		Future<String> result = someMethod.sayHello("nick");
+        System.out.println("after call sayHello");		
+		while(!result.isDone()) { // 等待结果返回
+			System.out.println("result:" + result.get());
+		}
+	}
+
+}
