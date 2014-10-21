e49f59d039a27b68624af31877baaa3f201e3d1e
pugwoo <pugwoo@gmail.com>
2014-10-21 18:17:48 +0800

    可以动态增加task

--- spring_basic/src/test/java/spring_basic/aop/test/TestTask.java
+++ spring_basic/src/test/java/spring_basic/aop/test/TestTask.java
@@ -2,6 +2,7 @@ package spring_basic.aop.test;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 /**
  * 2014年10月14日 20:26:49
@@ -13,6 +14,14 @@ public class TestTask {
 		ApplicationContext context = new ClassPathXmlApplicationContext(xmlPath);
 
 		System.out.println(context);
+		
+		// 拿到scheduler之后，可以控制一些任务的执行
+		ThreadPoolTaskScheduler scheduler =
+				(ThreadPoolTaskScheduler) context.getBean("taskScheduler");
+
+        // 可以调用scheduler的execute方法动态新增一个task
+		
+		// 好像无法remove已经在执行的任务，也无法查看到当前在执行的任务
 		
 	}
 
