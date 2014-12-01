20ea851a61d9638dc7547ed0ce2cfe8ee9c8314f
pugwoo <pugwoo@gmail.com>
2014-12-01 14:46:17 +0800

    更好的写法

--- spring_mvc3/src/main/java/spring_mvc3/web/NewCarController.java
+++ spring_mvc3/src/main/java/spring_mvc3/web/NewCarController.java
@@ -7,9 +7,9 @@ import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import spring_mvc3.entity.Car;
 import spring_mvc3.service.BrandManager;
@@ -34,10 +34,9 @@ public class NewCarController {
 	 * GET调用
 	 */
 	@RequestMapping(value = "/new_car", method = RequestMethod.GET)
-	public ModelAndView getRequest() {
-		ModelAndView modelAndView = new ModelAndView("newCar");
-		modelAndView.addObject("brands", brandManager.getAll());
-		return modelAndView;
+	public String getRequest(Model model) {
+		model.addAttribute("brands", brandManager.getAll());
+		return "newCar";
 	}
 
 	/**
@@ -45,7 +44,8 @@ public class NewCarController {
 	 * 参数可以是：@RequestParam(value = "model", required = false) String model
 	 */
 	@RequestMapping(value = "/new_car", method = RequestMethod.POST)
-	public ModelAndView postRequest(String model, BigDecimal price,
+	public String postRequest(Model modelView, // 由于名字冲突，所以换了个名字
+			String model, BigDecimal price,
 			/*@RequestParam("brand") Long brandId*/ Long brand) {
 		
 		Long brandId = brand;
@@ -61,19 +61,17 @@ public class NewCarController {
 
 		carManager.add(car);
 
-		ModelAndView modelAndView = new ModelAndView("newCarResult");
-		modelAndView.addObject("car", car);
-		return modelAndView;
+		modelView.addAttribute("car", car);
+		return "newCarResult";
 	}
 
 	/**
 	 * GET调用
 	 */
 	@RequestMapping(value = "/new_car_ognl", method = RequestMethod.GET)
-	public ModelAndView getRequestOGNL() {
-		ModelAndView modelAndView = new ModelAndView("newCarOGNL");
-		modelAndView.addObject("brands", brandManager.getAll());
-		return modelAndView;
+	public String getRequestOGNL(Model model) {
+		model.addAttribute("brands", brandManager.getAll());
+		return "newCarOGNL";
 	}
 
 	/**
@@ -84,14 +82,12 @@ public class NewCarController {
 	 * 【同名问题】当方法执行完之后，Spring又把参数car放入到request中，导致car被覆盖，使用ModelAndView可以解决
 	 */
 	@RequestMapping(value = "/new_car_ognl", method = RequestMethod.POST)
-	public ModelAndView postRequestOGNL(Car car) {
+	public String postRequestOGNL(Model model, Car car) {
 
 		carManager.add(car);
 
-		ModelAndView modelAndView = new ModelAndView("newCarResult");
-		modelAndView.addObject("car", car);
-
-		return modelAndView;
+		model.addAttribute("car", car);
+		return "newCarResult";
 	}
 
 	public BrandManager getBrandManager() {
