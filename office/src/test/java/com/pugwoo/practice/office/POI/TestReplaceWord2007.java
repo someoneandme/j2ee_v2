fa05c7e9a5908da76b0a1c34f60c04a02b61cbd0
pugwoo <pugwoo@gmail.com>
2014-12-22 18:03:23 +0800

    word 2007 长tag替换实验，成功

--- office/src/test/java/com/pugwoo/practice/office/POI/TestReplaceWord2007.java
+++ office/src/test/java/com/pugwoo/practice/office/POI/TestReplaceWord2007.java
@@ -19,24 +19,70 @@ import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 /**
  * 2014年12月18日 09:18:48
  * 
- * 关于表格的处理：
- * 1. 增加一行：
- * 2. 增加和当前行相同格式的一行：
- * http://stackoverflow.com/questions/16645344/xwpftablerow-adding-new-row-with-current-style
+ * 关于表格的处理： 1. 增加一行： 2. 增加和当前行相同格式的一行：
+ * http://stackoverflow.com/questions/16645344
+ * /xwpftablerow-adding-new-row-with-current-style
  * 
  * XWPFTable 有addRow和createRow的方法
  */
 public class TestReplaceWord2007 {
 
+	private static void replace(XWPFParagraph paragraph, String searchValue,
+			String replacement) {
+		if (hasReplaceableItem(paragraph.getText(), searchValue)) {
+			String replacedText = paragraph.getText().replace(searchValue,
+					replacement);
+			removeAllRuns(paragraph);
+			insertReplacementRuns(paragraph, replacedText);
+		}
+	}
+
+	private static void insertReplacementRuns(XWPFParagraph paragraph,
+			String replacedText) {
+		String[] replacementTextSplitOnCarriageReturn = replacedText.split("\n");
+
+		for (int j = 0; j < replacementTextSplitOnCarriageReturn.length; j++) {
+			String part = replacementTextSplitOnCarriageReturn[j];
+
+			XWPFRun newRun = paragraph.insertNewRun(j);
+			newRun.setText(part);
+
+			if (j + 1 < replacementTextSplitOnCarriageReturn.length) {
+				newRun.addCarriageReturn();
+			}
+		}
+	}
+
+	private static void removeAllRuns(XWPFParagraph paragraph) {
+		int size = paragraph.getRuns().size();
+		for (int i = 0; i < size; i++) {
+			paragraph.removeRun(0);
+		}
+	}
+
+	private static boolean hasReplaceableItem(String runText, String searchValue) {
+		return runText.contains(searchValue);
+	}
+
 	private static void processParagraphs(List<XWPFParagraph> paragraphList,
 			String findText, String replaceText) {
 		for (XWPFParagraph paragraph : paragraphList) {
-			List<XWPFRun> runs = paragraph.getRuns();
-			for (XWPFRun run : runs) {
-				String text = run.getText(0);
-				text = text.replace(findText, replaceText);
-				run.setText(text, 0);
-			}
+			/**
+			 * 在实际使用中，下面的替换方法还是有些问题 在同一个表格中，一个paragraph被拆成多个Runs
+			 * 导致我要替换的字符串被拆成多个Runs了 所以替换时是找不到需要替换的文本了
+			 * 
+			 * TODO 看这里能不能封装成一个比较好的工具
+			 * 
+			 * 成功了! 参考:
+			 * http://stackoverflow.com/questions/22268898/replacing-a-text-in-apache-poi-xwpf
+			 */
+//			List<XWPFRun> runs = paragraph.getRuns();
+//			for (XWPFRun run : runs) {
+//				String text = run.getText(0);
+//				text = text.replace(findText, replaceText);
+//				run.setText(text, 0);
+//			}
+			replace(paragraph, findText, replaceText);
 		}
 	}
 
@@ -45,23 +91,24 @@ public class TestReplaceWord2007 {
 		// Paragraph treatment
 		List<XWPFParagraph> paragraphList = doc.getParagraphs();
 		processParagraphs(paragraphList, findText, replaceText);
-		
+
 		// Processing table 处理word2007文档中的表格
 		Iterator<XWPFTable> it = doc.getTablesIterator();
 		while (it.hasNext()) {
 			XWPFTable table = it.next();
 			List<XWPFTableRow> rows = table.getRows();
-			
+
 			XWPFTableRow newRow = table.createRow(); // 这样就创建了一行并加入到表格后面了!!
-//			table.addRow(newRow);  // 如果这里放开，就相当于执行了两遍了
+			// table.addRow(newRow); // 如果这里放开，就相当于执行了两遍了
 			System.out.println("add new row"); // 只执行了一次
 			// 插入新的一行，很奇怪加上这两行之后，表格多了2行.
 			// 这里只执行了一次
-			
+
 			for (XWPFTableRow row : rows) {
 				List<XWPFTableCell> cells = row.getTableCells();
 				for (XWPFTableCell cell : cells) {
-					List<XWPFParagraph> paragraphListTable = cell.getParagraphs();
+					List<XWPFParagraph> paragraphListTable = cell
+							.getParagraphs();
 					processParagraphs(paragraphListTable, findText, replaceText);
 				}
 			}
@@ -70,14 +117,15 @@ public class TestReplaceWord2007 {
 
 	public static void main(String[] args) throws IOException {
 		String outputPath = "C:/output_2007.docx";
-		
+
 		String dataDOC = "/poi/a.docx";
-		InputStream in = new BufferedInputStream(TestReplaceWord2007
-				.class.getResourceAsStream(dataDOC));
+		InputStream in = new BufferedInputStream(
+				TestReplaceWord2007.class.getResourceAsStream(dataDOC));
 
 		XWPFDocument doc = new XWPFDocument(in);
-		
-		replaceText(doc, "{XXXXX_NO}", "我的编号112233");
+
+		//replaceText(doc, "{XXXXX_NO}", "我的编号112233");
+		replaceText(doc, "{this_is_a_long_tag_in_a_table}", "2222233333");
 
 		FileOutputStream fos = new FileOutputStream(outputPath);
 		doc.write(fos);
