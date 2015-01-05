d85a6e62a720d66fce4eecccd20e0e1a6831fae1
pugwoo <pugwoo@gmail.com>
2015-01-05 11:47:12 +0800

    更新fill PDF代码

--- office/src/test/java/com/pugwoo/practice/office/itext/TestFillPDF.java
+++ office/src/test/java/com/pugwoo/practice/office/itext/TestFillPDF.java
@@ -1,13 +1,12 @@
 package com.pugwoo.practice.office.itext;
 
+import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import com.lowagie.text.DocumentException;
-import com.lowagie.text.Font;
 import com.lowagie.text.pdf.AcroFields;
-import com.lowagie.text.pdf.BaseFont;
-import com.lowagie.text.pdf.PdfContentByte;
 import com.lowagie.text.pdf.PdfReader;
 import com.lowagie.text.pdf.PdfStamper;
 
@@ -21,18 +20,16 @@ import com.lowagie.text.pdf.PdfStamper;
 public class TestFillPDF {
 
 	public static void main(String[] args) throws IOException, DocumentException {
-		PdfReader reader = new PdfReader("c:/学生成绩登记表 - form.pdf");
+        String dataXLS = "/itext/学生成绩登记表 - form.pdf";
+        InputStream inputXML = new BufferedInputStream(TestFillPDF.class
+                .getResourceAsStream(dataXLS));
+		
+		PdfReader reader = new PdfReader(inputXML);
 		PdfStamper stamp = new PdfStamper(reader, new FileOutputStream("c:/output.pdf"));
-		
-		// TODO 1表示第一页，这个例子可能只处理第一页的情况
-//		PdfContentByte under = stamp.getUnderContent(1);
-		
-		// 中文字体
-//		BaseFont baseFont = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);
-//		Font fontChinese = new Font(baseFont, 12, Font.NORMAL);
 		
 		// 取出报表模版中的所有字段
 		AcroFields form = stamp.getAcroFields();
+		
 		// 为字段赋值，注意字段名称是区分大小写的
 		form.setField("stu_no_1", "372102");
 		form.setField("stu_name_1", "nick xie");
