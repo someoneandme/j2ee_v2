320977b6eee383cb3f552ff4ee4aa10a7c8cfb34
pugwoo <pugwoo@gmail.com>
2015-05-20 11:26:00 +0800

    调整代码结构

--- ibatis/src/test/java/com/pugwoo/practice/ibatis/TestOperatingDB.java
+++ ibatis/src/test/java/com/pugwoo/practice/ibatis/TestOperatingDB.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.Reader;
 import java.sql.SQLException;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 import com.ibatis.common.resources.Resources;
 import com.ibatis.sqlmap.client.SqlMapClient;
 import com.ibatis.sqlmap.client.SqlMapClientBuilder;
@@ -15,15 +18,24 @@ import com.pugwoo.practice.ibatis.entity.Student;
  *
  */
 public class TestOperatingDB {
-
-	public static void main(String[] args) throws SQLException, IOException {
+	
+	private static SqlMapClient sqlMapClient;
+	
+	// 说明，@BeforeClass在Class构建时执行一次，@Before在每次@Test方法执行前执行一次
+	@BeforeClass
+	public static void before() throws IOException {
 		Reader reader = Resources.getResourceAsReader("SqlMapConfig.xml");
-		SqlMapClient sqlMap = SqlMapClientBuilder.buildSqlMapClient(reader);
-		
-		Student student = (Student) sqlMap.queryForObject("Student.getStudentById", 1);
+		sqlMapClient = SqlMapClientBuilder.buildSqlMapClient(reader);
+	}
+	
+	@Test
+	public void testSelect() throws SQLException {
+		Student student = (Student) sqlMapClient.queryForObject(
+				"Student.getStudentById", 3);
 		System.out.println(student);
 		
 		// 启用namespace之后就只能用上面那种，一般大型项目都会用namespace的
+		// 没有用namespace的如下：
 //		student = (Student) sqlMap.queryForObject("getStudentById", 1);
 //		System.out.println(student);
 	}
