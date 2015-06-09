74da0448bda893d440fd0e8e703e6c06ef473433
pugwoo <pugwoo@gmail.com>
2015-06-09 08:12:34 +0800

    整理代码，抽取出有用的部分，实际上那些命令行示例程序，是将view层和逻辑层混在一起了

--- lucene/src/main/java/com/pugwoo/practice/lucene/dir_index/SearchFiles.java
+++ lucene/src/main/java/com/pugwoo/practice/lucene/dir_index/SearchFiles.java
@@ -27,9 +27,7 @@ public class SearchFiles {
 		// 查询参数
 		String field = "contents"; // 查询的域field
 		String queryString = "hello"; // 要查询的关键词
-		String queries = null; // 要查询的文件名?
-		boolean raw = false;
-		int hitsPerPage = 10;
+		int hitsPerPage = 10; // 分页
 		
 		IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths
 				.get(index)));
@@ -39,13 +37,14 @@ public class SearchFiles {
 		QueryParser parser = new QueryParser(field, analyzer);
 		Query query = parser.parse(queryString);
 		
+		// 关于分页，实际上search应该是记录了当前位置，每次调用都向后取n条记录
 		TopDocs results = searcher.search(query, hitsPerPage); // 这里只查第一页
 		System.out.println(results.totalHits + " total matching documents");
 		
 		ScoreDoc[] hits = results.scoreDocs;
 		for(ScoreDoc hit : hits) {
 			Document doc = searcher.doc(hit.doc); // 这里就读取到索引时的Document了
-			System.out.println(doc);
+			System.out.println("score:" +  hit.score + "=" + doc);
 		}
 		
 		reader.close();
