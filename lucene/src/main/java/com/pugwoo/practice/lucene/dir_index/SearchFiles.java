e6bbbc49e5d098a0776b86d9b4b1921076457fbb
pugwoo <pugwoo@gmail.com>
2015-06-09 11:36:11 +0800

    分页的方式

--- lucene/src/main/java/com/pugwoo/practice/lucene/dir_index/SearchFiles.java
+++ lucene/src/main/java/com/pugwoo/practice/lucene/dir_index/SearchFiles.java
@@ -27,7 +27,7 @@ public class SearchFiles {
 		// 查询参数
 		String field = "contents"; // 查询的域field
 		String queryString = "hello"; // 要查询的关键词
-		int hitsPerPage = 10; // 分页
+		int hitsPerPage = 2; // 分页
 		
 		IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths
 				.get(index)));
@@ -37,12 +37,19 @@ public class SearchFiles {
 		QueryParser parser = new QueryParser(field, analyzer);
 		Query query = parser.parse(queryString);
 		
-		// 关于分页，实际上search应该是记录了当前位置，每次调用都向后取n条记录
-		TopDocs results = searcher.search(query, hitsPerPage); // 这里只查第一页
+		// 读取n条记录，默认score高的在前
+		TopDocs results = searcher.search(query, hitsPerPage); 
 		System.out.println(results.totalHits + " total matching documents");
 		
-		ScoreDoc[] hits = results.scoreDocs;
-		for(ScoreDoc hit : hits) {
+		for(ScoreDoc hit : results.scoreDocs) {
+			Document doc = searcher.doc(hit.doc); // 这里就读取到索引时的Document了
+			System.out.println("score:" +  hit.score + "=" + doc);
+		}
+		
+		// 查找下一页
+		results = searcher.searchAfter(results.scoreDocs[results.scoreDocs.length - 1],
+				query, hitsPerPage);
+		for(ScoreDoc hit : results.scoreDocs) {
 			Document doc = searcher.doc(hit.doc); // 这里就读取到索引时的Document了
 			System.out.println("score:" +  hit.score + "=" + doc);
 		}
