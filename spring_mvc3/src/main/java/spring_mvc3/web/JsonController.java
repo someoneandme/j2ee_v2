b6bd2abc23440bc1bb4aaa38251e54c278ea4025
pugwoo <pugwoo@gmail.com>
2015-08-15 11:29:12 +0800

    支持json输出

--- spring_mvc3/src/main/java/spring_mvc3/web/JsonController.java
+++ spring_mvc3/src/main/java/spring_mvc3/web/JsonController.java
@@ -3,6 +3,8 @@ package spring_mvc3.web;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,14 +12,30 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 演示输入纯文本
+ * 
+ * 直接使用@ResponseBody来输出数据，需要加入json解析工具依赖，推荐fastjson
+ * 默认只要导入jackson的依赖，不需要额外配置就可以使用@ResponseBody的功能
+ * 但因为fastjson性能更佳，为了用fastjson，还需要配置mvc:annotation-driven的mvc:message-converters
  */
 @Controller
 public class JsonController {
 
+	@ResponseBody // 加这个注解
 	@RequestMapping("/json")
+	public Map<String, Object> json() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("id", 3);
+		map.put("name", "参数\"");
+		return map;
+	}
+	
+	//////////////////下面的这些方法都太老土，性能差，不安全，不推荐使用，仅适用于写些数据///////////////////////
+	
+	@RequestMapping("/json_old")
 	public void json(Writer writer) throws IOException {
 		// 假设json是通过工具获得的jsonString
 		String json = "{id:3}";
