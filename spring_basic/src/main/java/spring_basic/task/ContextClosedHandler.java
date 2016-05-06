7ee4e1b8004b5ca56f93c25520f8b931a1e35bf4
pugwoo <pugwoo@gmail.com>
2016-05-06 11:18:39 +0800

    ====

--- spring_basic/src/main/java/spring_basic/task/ContextClosedHandler.java
+++ spring_basic/src/main/java/spring_basic/task/ContextClosedHandler.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 class ContextClosedHandler implements ApplicationListener<ContextClosedEvent> {
+	
 	@Autowired
 	ThreadPoolTaskExecutor executor;
 	
