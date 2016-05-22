161a0f15416c031d482b509a84622868ccfd6b2d
pugwoo <pugwoo@gmail.com>
2016-05-22 17:48:57 +0800

    fastjson说明

--- spring_mvc3/src/main/java/spring_mvc3/web/JsonController.java
+++ spring_mvc3/src/main/java/spring_mvc3/web/JsonController.java
@@ -45,6 +45,8 @@ public class JsonController {
 	}
 	
 	// 经过测试，string也不会被fastjson处理，会直接按string的值输出到前端
+	// 【注意】后来我发现spring会不会被fastjson处理这个情况，和fastjson的版本有关，1.2.6就不会，1.2.11新版就会
+	// 这个东西也没有说合理不合理的问题，只是要注意一下，1.2.10版本还不会处理string
 	@ResponseBody
 	@RequestMapping("/json_string")
 	public String testString() {
